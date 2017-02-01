0e8557b3eef361a1193c7687a225914ef87c0f4e
Tino Wagner <ich@tinowagner.com>
2017-02-01 01:21:06 +0100

    Add App icon and tweak document icons

--- icons/build-app-iconset.sh
+++ icons/build-app-iconset.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 #
-# Create a document iconset for Jupyter notebook from an SVG template
+# Create an App iconset for Jupyter Notebook Viewer from an SVG template
 #
-base_icon=/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericDocumentIcon.icns
-target_icon_name="Jupyter Notebook Document"
-svg_icon_file=$(pwd)/ipynb-document.svg
-iconutil --convert iconset --output "${target_icon_name}.iconset" "${base_icon}"
+target_folder="$(pwd)/../Jupyter Notebook Viewer/Images.xcassets/AppIcon.appiconset"
+svg_icon_file=$(pwd)/viewer-app.svg
 iconsizes="16 32 128 256 512"
 for size in $iconsizes ; do
     echo $size
 
-    output_name="$(pwd)/${target_icon_name}.iconset/icon_${size}x${size}.png"
-    inkscape ${svg_icon_file} --export-id-only --export-id=v${size} \
+    output_name="${target_folder}/icon_${size}x${size}.png"
+    inkscape ${svg_icon_file} \
         --export-png="${output_name}" \
         --export-width=$(echo 4*$size | bc) --export-height=$(echo 4*$size | bc)
     convert "${output_name}" -colorspace RGB +sigmoidal-contrast "6.5,50%" \
         -filter Lanczos -resize "${size}x${size}" \
         -sigmoidal-contrast "6.5,50%" -colorspace sRGB "${output_name}"
 
-    output_name="$(pwd)/${target_icon_name}.iconset/icon_${size}x${size}@2x.png"
-    inkscape ${svg_icon_file} --export-id-only --export-id=v${size}x2 \
+    output_name="${target_folder}/icon_${size}x${size}@2x.png"
+    inkscape ${svg_icon_file} \
         --export-png="${output_name}" \
         --export-width=$(echo 4*$size | bc) --export-height=$(echo 4*$size | bc)
     convert "${output_name}" -colorspace RGB +sigmoidal-contrast "6.5,50%" \
         -filter Lanczos -resize $(echo 2*$size | bc)x$(echo 2*$size | bc) \
         -sigmoidal-contrast "6.5,50%" -colorspace sRGB "${output_name}"
 done
-pushd "${target_icon_name}.iconset"
+pushd "${target_folder}"
 optipng *.png
 popd
