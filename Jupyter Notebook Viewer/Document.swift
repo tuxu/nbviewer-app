797001d3faff09a2c331cefdd2b92c38bf919a42
Tino Wagner <ich@tinowagner.com>
2017-10-19 00:53:15 +0200

    Migrate to Xcode 9 / Swift 4

--- Jupyter Notebook Viewer/Document.swift
+++ Jupyter Notebook Viewer/Document.swift
@@ -16,14 +16,14 @@ class Document: NSDocument {
         super.init()
     }
 
-    override class func autosavesInPlace() -> Bool {
+    override class var autosavesInPlace: Bool {
         return false
     }
 
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: "Main", bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "Document Window Controller")) as! NSWindowController
         self.addWindowController(windowController)
     }
 
